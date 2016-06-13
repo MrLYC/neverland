f66047d0f4e7ed27d02f7606217adf56944f4879
MrLYC <imyikong@gmail.com>
2016-06-13 14:47:30 +0800

    update keeper

--- keeper/models.py
+++ keeper/models.py
@@ -5,15 +5,17 @@ class Keeper(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, null=False, blank=False)
     content_type = models.CharField(
-        max_length=255, choices=(
+        max_length=255, null=True, blank=True, choices=(
             ('application/json', 'application/json'),
             ('text/html', 'text/html'),
             ('text/plain', 'text/plain'),
             ('application/octet-stream', 'application/octet-stream'),
         )
     )
-    data = models.CharField(max_length=5120)
-    token = models.CharField(max_length=16, null=True, blank=True)
+
+    data = models.CharField(max_length=5120, null=True, blank=True)
+    read_token = models.CharField(max_length=16, null=True, blank=True)
+    write_token = models.CharField(max_length=16, null=True, blank=True)
     charset = models.CharField(max_length=16, null=True, blank=True)
     reason = models.CharField(max_length=255, null=True, blank=True)
     status = models.IntegerField(null=True, blank=True)
--- keeper/views.py
+++ keeper/views.py
@@ -9,6 +9,9 @@ class KeeperView(generic.View):
 
     def get(self, request, path):
         keeper = get_object_or_404(models.Keeper, name=path)
+        read_token = keeper.read_token
+        if read_token and read_token != request.GET.get("read_token"):
+            return http.HttpResponseNotFound()
 
         response_params = {"content": keeper.data}
         if keeper.content_type:
@@ -24,15 +27,38 @@ class KeeperView(generic.View):
 
     def post(self, request, path):
         keeper = get_object_or_404(models.Keeper, name=path)
-        if keeper.token and keeper.token != request.META.get("HTTP_X_TOKEN"):
+        write_token = request.META.get("HTTP_WRITE_TOKEN")
+        if keeper.write_token and keeper.write_token != write_token:
             raise http.Http404()
 
         keeper.data = request.body
-        keeper.reason = request.GET.get("reason")
-        keeper.status = request.GET.get("status")
-        keeper.charset = request.GET.get("charset")
-        keeper.content_type = request.META.get("CONTENT_TYPE")
+
+        write_token = request.META.get("HTTP_NEWWRITE_TOKEN")
+        if write_token is not None:
+            keeper.write_token = write_token
+
+        read_token = equest.META.get("HTTP_READ_TOKEN")
+        if read_token is not None:
+            keeper.read_token = read_token
+
+        reason = request.GET.get("reason")
+        if reason is not None:
+            keeper.reason = reason
+
+        status = request.GET.get("status")
+        if status is not None:
+            keeper.status = status
+
+        charset = request.GET.get("charset")
+        if charset is not None:
+            keeper.charset = charset
+
+        content_type = request.META.get("CONTENT_TYPE")
+        if content_type is not None:
+            keeper.content_type = content_type
+
         keeper.save()
+
         return http.HttpResponse(
             keeper.data, reason=keeper.reason, charset=keeper.charset,
             status=keeper.status, content_type=keeper.content_type,
