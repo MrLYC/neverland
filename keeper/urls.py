ccc8f8a75f6a4528e5a264e2b01f0ae785eb8adf
刘奕聪 <imyikong@gmail.com>
2017-07-06 23:44:39 +0800

    update path

--- keeper/urls.py
+++ keeper/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<path>[\w/\-]+)/?$', views.KeeperView.as_view()),
+    url(r'^(?P<path>.+?)/?$', views.KeeperView.as_view()),
 ]
