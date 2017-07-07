9393e710bbd763945170afdad42df93e6707e25d
刘奕聪 <imyikong@gmail.com>
2017-07-07 20:08:28 +0800

    update content type

--- keeper/models.py
+++ keeper/models.py
@@ -2,6 +2,65 @@ from django.db import models
 from django.utils.timezone import now
 
 
+CONTENT_TYPE_CHOICES = (
+    ('text/plain', 'txt'),
+    ('text/html', 'html'),
+    ('text/css', 'css'),
+    ('application/javascript', 'js'),
+    ('application/json', 'json'),
+    ('image/jpeg', 'jpeg'),
+    ('image/x-icon', 'ico'),
+    ('image/gif', 'gif'),
+    ('application/xml', 'xml'),
+    ('application/octet-stream', 'bin'),
+    ('audio/aac', 'aac'),
+    ('application/x-abiword', 'abw'),
+    ('application/octet-stream', 'arc'),
+    ('video/x-msvideo', 'avi'),
+    ('application/vnd.amazon.ebook', 'azw'),
+    ('application/x-bzip', 'bz'),
+    ('application/x-bzip2', 'bz2'),
+    ('application/x-csh', 'csh'),
+    ('text/csv', 'csv'),
+    ('application/msword', 'doc'),
+    ('application/epub+zip', 'epub'),
+    ('text/calendar', 'ics'),
+    ('application/java-archive', 'jar'),
+    ('audio/midi', 'mid'),
+    ('video/mpeg', 'mpeg'),
+    ('application/vnd.apple.installer+xml', 'mpkg'),
+    ('application/vnd.oasis.opendocument.presentation', 'odp'),
+    ('application/vnd.oasis.opendocument.spreadsheet', 'ods'),
+    ('application/vnd.oasis.opendocument.text', 'odt'),
+    ('audio/ogg', 'oga'),
+    ('video/ogg', 'ogv'),
+    ('application/ogg', 'ogx'),
+    ('application/pdf', 'pdf'),
+    ('application/vnd.ms-powerpoint', 'ppt'),
+    ('application/x-rar-compressed', 'rar'),
+    ('application/rtf', 'rtf'),
+    ('application/x-sh', 'sh'),
+    ('image/svg+xml', 'svg'),
+    ('application/x-shockwave-flash', 'swf'),
+    ('application/x-tar', 'tar'),
+    ('image/tiff', 'tif'),
+    ('application/x-font-ttf', 'ttf'),
+    ('application/vnd.visio', 'vsd'),
+    ('audio/x-wav', 'wav'),
+    ('audio/webm', 'weba'),
+    ('video/webm', 'webm'),
+    ('image/webp', 'webp'),
+    ('application/x-font-woff', 'woff'),
+    ('application/xhtml+xml', 'xhtml'),
+    ('application/vnd.ms-excel', 'xls'),
+    ('application/vnd.mozilla.xul+xml', 'xul'),
+    ('application/zip', 'zip'),
+    ('video/3gpp', '3gp'),
+    ('video/3gpp2', '3g2'),
+    ('application/x-7z-compressed', '7z')
+)
+
+
 class Keeper(models.Model):
     update_at = models.DateTimeField(auto_now=True)
 
@@ -12,12 +71,7 @@ class Keeper(models.Model):
         max_length=255, null=False, blank=False, unique=True,
     )
     c_type = models.CharField(
-        max_length=255, null=True, blank=True, choices=(
-            ('application/json', 'application/json'),
-            ('text/html', 'text/html'),
-            ('text/plain', 'text/plain'),
-            ('application/octet-stream', 'application/octet-stream'),
-        )
+        max_length=255, null=True, blank=True, choices=CONTENT_TYPE_CHOICES,
     )
 
     data = models.TextField(max_length=1024 * 20, null=True, blank=True)
